27ac3cfa66b4a3e75a755fcb3138a2e94339580d
anhthii <nguyendanganhthi247@gmail.com>
2023-08-04 11:19:23 +0700

    Add mumbai TIXS

--- src/buildList.js
+++ src/buildList.js
@@ -3,30 +3,58 @@ const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
 const kovan = require("./tokens/kovan.json");
 const polygon = require("./tokens/polygon.json");
+const mumbai = require("./tokens/mumbai.json");
 
 module.exports = async function buildList() {
   let dedupe = { n: [], s: [], a: [] };
   let i = 0;
-  let { body: { tokens } } = await got.get("https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/tokenlist.json", { responseType: "json" });
-  tokens = tokens.map(
-    ({ name, chainId, address, symbol, decimals, logoURI }) => {
-      const n = `${ chainId || 1 }#${name}`;
-      const s = `${ chainId || 1 }#${symbol}`;
-      const a = `${ chainId || 1 }#${address}`;
-      
-      if (dedupe.n.includes(n) || dedupe.s.includes(s) || dedupe.a.includes(a)) return null;
-      else if (!name || !symbol || !address || !decimals || decimals <= 0 || !logoURI) return null;
-      else if (!/^[a-zA-Z0-9+\-%/\$]+$/.test(symbol) || !/^[ \w.'+\-%/À-ÖØ-öø-ÿ:]+$/.test(name)) return null;
-      dedupe.n.push(n); dedupe.s.push(s); dedupe.a.push(a);
+  let {
+    body: { tokens },
+  } = await got.get(
+    "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/tokenlist.json",
+    { responseType: "json" }
+  );
+  tokens = tokens
+    .map(({ name, chainId, address, symbol, decimals, logoURI }) => {
+      const n = `${chainId || 1}#${name}`;
+      const s = `${chainId || 1}#${symbol}`;
+      const a = `${chainId || 1}#${address}`;
+
+      if (dedupe.n.includes(n) || dedupe.s.includes(s) || dedupe.a.includes(a))
+        return null;
+      else if (
+        !name ||
+        !symbol ||
+        !address ||
+        !decimals ||
+        decimals <= 0 ||
+        !logoURI
+      )
+        return null;
+      else if (
+        !/^[a-zA-Z0-9+\-%/\$]+$/.test(symbol) ||
+        !/^[ \w.'+\-%/À-ÖØ-öø-ÿ:]+$/.test(name)
+      )
+        return null;
+      dedupe.n.push(n);
+      dedupe.s.push(s);
+      dedupe.a.push(a);
 
       // remove non checksummed tokens
       if (address.toLowerCase() === address) return null;
-      
-      return { name, address, symbol, decimals, chainId: chainId || 1, logoURI };
-    }
-  ).filter(Boolean);
+
+      return {
+        name,
+        address,
+        symbol,
+        decimals,
+        chainId: chainId || 1,
+        logoURI,
+      };
+    })
+    .filter(Boolean);
   dedupe = null;
-  
+
   const parsed = version.split(".");
   return {
     name: "IXS Default List",
@@ -38,7 +66,7 @@ module.exports = async function buildList() {
     },
     tags: {},
     keywords: ["ixs", "default"],
-    tokens: [...tokens, ...mainnet, ...kovan, ...polygon]
+    tokens: [...tokens, ...mainnet, ...kovan, ...polygon, ...mumbai]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
